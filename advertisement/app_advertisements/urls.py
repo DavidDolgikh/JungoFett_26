52c736feb29acbff41dc8d03909eae3760cae960
Dave <dave@gmail.com>
2023-07-12 12:31:34 +0800

    Homework_lesson_5

--- advertisement/app_advertisements/urls.py
+++ advertisement/app_advertisements/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
 urlpatterns = [         # обязательно, зарезервированное имя, которое фреймворк будет искать
-    path('', index)     # эта функция формирует пути (связывать путь с представлением (корневым адресом))
+    path('', index, name='main-page'),     # эта функция формирует пути (связывать путь с представлением (корневым адресом))
+    path('top-sellers/', top_sellers, name='top-sellers')
 ]
