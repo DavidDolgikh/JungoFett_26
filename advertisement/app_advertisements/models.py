fb543bf3ef6876790c3254021bcfda493efd367c
Dave <dave@gmail.com>
2023-08-02 09:08:05 +0800

    Homework_lesson_8

--- advertisement/app_advertisements/models.py
+++ advertisement/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class Advertisement(models.Model):
@@ -14,6 +16,26 @@ class Advertisement(models.Model):
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.create_at.date() == timezone.now().date():
+            created_time = self.create_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.create_at.strftime('%d.%m.%y')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.update_at.date() == timezone.now().date():
+            updated_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', updated_time
+             )
+        return self.update_at.strftime('%d.%m.%y')
 
 
 
